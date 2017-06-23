eb88176ca5b8730858abe8dfdf11db3df07281b0
Gil Or <orgil7@gmail.com>
2017-06-23 17:00:51 +0300

    temp1

--- run_me.py
+++ run_me.py
@@ -2,8 +2,15 @@ import os
 tt=os.system('ls')
 print(tt)
 listfile_name = 'd:/to_be_backup/temp_2016.txt'
-print ("hello")
+prefix = 'd:/to_be_backup/2016/'
+print ("helloddd")
 f = open(listfile_name, 'r')
 data = f.readlines()
 for filen in data:
-	print(filen)
+	gg=filen.rstrip('\n')
+	full_path=prefix+gg
+	print([full_path])
+	cmd = "cp "+full_path+" ."
+	print([cmd])
+	os.system(cmd)
+
